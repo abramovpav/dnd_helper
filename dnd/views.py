1687a34b19639a1fe852ed000d7489cfbcd32a69
Pavel Abramau <abramovpav@gmail.com>
2017-11-18 09:56:46 -0500

    Added scss builder. Added initial styles. Updated list of templates context processors

--- dnd/views.py
+++ dnd/views.py
@@ -1,11 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
 from django.views.generic import TemplateView
-
-from dnd_helper.settings import STATIC_URL
 
 
 class DnDHelperMainView(LoginRequiredMixin, TemplateView):
@@ -16,8 +13,7 @@ class DnDHelperMainView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         ctx = {
-            'BASE_URL': reverse('react-application'),
-            'STATIC_URL': STATIC_URL,
+            'BASE_URL': reverse('react-application')
         }
         kwargs.update(ctx)
         return super(DnDHelperMainView, self).get_context_data(**kwargs)
